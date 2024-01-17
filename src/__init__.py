f57f01075173bc7a49dd443d03203921d6cfc210
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-01-17 11:29:07 +0100

    Linting and initial formats

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from .custom_format import *
+from .french_gps_coordinates import FrenchGPSCoordinates as FrenchGPSCoordinates
+from .siren_format import Siren as Siren
+from .siret_format import Siret as Siret
