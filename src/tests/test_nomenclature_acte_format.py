e946887bbdcbc84e3afdfd78c08467f24f2edcc9
Amélie Rondot <amelie.rondot@multi.coop>
2024-01-19 12:10:49 +0100

    Refacto test is_valid()

--- src/tests/test_nomenclature_acte_format.py
+++ src/tests/test_nomenclature_acte_format.py
@@ -1,15 +1,6 @@
 from frformat.nomenclature_acte_format import AUTHORIZED_VALUES, EXTRA_SPACE, INVALID_PREFIX, NomenclatureActe, MISSING_SLASH
 
 
-def test_nomenclature_acte_value():
-    acte_value = "Fonction publique/foobar"
-    assert NomenclatureActe.is_valid(acte_value)
-
-    test_cases = ["foobar", "Fonction publique /foobar" "Fonction publique"]
-    for tc in test_cases:
-        assert not NomenclatureActe.is_valid(tc)
-
-
 def test_is_valid_with_details():
     valid_prefix = AUTHORIZED_VALUES[0]
     invalid_prefix = "invalid"
@@ -58,4 +49,5 @@ def test_is_valid_with_details():
     ]
 
     for tc in test_cases:
+        assert NomenclatureActe.is_valid(tc["value"]) == tc["expected"][0] 
         assert NomenclatureActe.is_valid_with_details(tc["value"]) == tc["expected"]
