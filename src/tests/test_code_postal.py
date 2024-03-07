14ac91a22414ac9613bf908b3b841e39afe3fe12
Amélie Rondot <amelie.rondot@multi.coop>
2024-03-07 10:51:16 +0100

    Lint: format files

--- src/tests/test_code_postal.py
+++ src/tests/test_code_postal.py
@@ -4,6 +4,7 @@ from frformat import PostalCode
 def test_postal_code():
     value = "05560"
     assert PostalCode.is_valid(value)
+    assert PostalCode.format(value) == value
 
     value = "77777"
     assert not PostalCode.is_valid(value)
