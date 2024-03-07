d6516438769950e763e7908657a9dd64313c8e73
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-03-07 10:29:25 +0100

    fixup! 🔵 rename files to english

--- src/frformat/__init__.py
+++ src/frformat/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
-from .code_postal import PostalCode as PostalCode
 from .custom_format import *
 from .french_gps_coordinates import FrenchGPSCoordinates as FrenchGPSCoordinates
 from .nomenclature_acte_format import NomenclatureActe as NomenclatureActe
+from .postal_code import PostalCode as PostalCode
 from .siren_format import Siren as Siren
 from .siret_format import Siret as Siret
