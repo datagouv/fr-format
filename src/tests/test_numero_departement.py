3ee1dcb1a72b798d79f93d0dd528818adcb41d60
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-04-03 17:01:15 +0200

    🟢 test "2a" "2b" lenient validation

--- src/tests/test_numero_departement.py
+++ src/tests/test_numero_departement.py
@@ -1,11 +1,21 @@
 from frformat import NumeroDepartement
+from tests.testing import validation_test_helper_factory
+
+aux_test_numero_departement = validation_test_helper_factory(NumeroDepartement)
 
 
 def test_numero_departement():
     test_cases = ["05", "2B", "974"]
+
+    aux_test_numero_departement(test_cases, isStrict=True, expectValid=True)
+
     for tc in test_cases:
-        assert NumeroDepartement.is_valid(tc)
         assert NumeroDepartement.format(tc) == tc
 
-    value = "99"
-    assert not NumeroDepartement.is_valid(value)
+    invalid_values = ["99", "2a", "2b"]
+    aux_test_numero_departement(invalid_values, isStrict=True, expectValid=False)
+
+    leniently_valid_values = ["2a", "2b"]
+    aux_test_numero_departement(
+        leniently_valid_values, isStrict=False, expectValid=True
+    )
