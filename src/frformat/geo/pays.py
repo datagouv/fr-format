6523fdd101baa29bf101a2e5bb48dc2d991f42e6
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-04-18 11:14:42 +0200

    feat: codes pays iso2 et iso3

--- src/frformat/geo/pays.py
+++ src/frformat/geo/pays.py
@@ -2,6 +2,6 @@ from frformat import enum_format
 from frformat.geo.pays_set import PAYS_SET
 
 name = "Pays et territoires étrangers"
-description = "Nom de pays et territoires étrangers selon COG2020"
+description = "Nom de pays et territoires étrangers selon COG2024"
 
 Pays = enum_format.new(name, description, PAYS_SET)
