7d83f9011fc8e45f4a2707d5f263569719f5542b
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-15 10:04:40 +0200

    feat: Format the given code using non-breaking spaces

--- src/frformat/code_rna.py
+++ src/frformat/code_rna.py
@@ -18,3 +18,7 @@ class CodeRNA(CustomStrFormat):
     @classmethod
     def is_valid(cls, value: str) -> bool:
         return bool(re.match(r"^W\d{9}$", value))
+
+    @classmethod
+    def _format(cls, value: str) -> str:
+        return f"{value[0:4]}\u00A0{value[4:7]}\u00A0{value[7:]}"
