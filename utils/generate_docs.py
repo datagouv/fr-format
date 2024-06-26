a814e4ac28c11f1068ff9546b299ec36657ba822
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2024-06-26 14:14:14 +0200

    merge 2 files

--- utils/generate_docs.py
+++ utils/generate_docs.py
@@ -1,5 +1,4 @@
 import jinja2
-from validators import generate_validators_documentation
 
 from frformat import (
     Canton,
@@ -39,16 +38,48 @@ all_validators = [
     Siret,
 ]
 
-TEMPLATE_FILE = "formats_template.md.jinja"
-OUTPUT_FILE = "./docs/formats.md"
 
-documentation = generate_validators_documentation(all_validators)
+def text_formatting(text):
+    new_text = text.strip()
+    formatted_description = ""
+    text_array = new_text.splitlines()
 
-template_loader = jinja2.FileSystemLoader(searchpath="./utils/")
-template_env = jinja2.Environment(loader=template_loader, trim_blocks=True)
-template = template_env.get_template(TEMPLATE_FILE)
+    if len(text_array) == 1:
+        formatted_description = text_array[0]
+        return formatted_description
 
-output_markdown = template.render(validators=documentation)
+    for line in text_array:
+        if line is text_array[0] and line != "":
+            formatted_description = line
+        else:
+            formatted_description = formatted_description + "<br>" + line
 
-with open(OUTPUT_FILE, "w") as f:
-    f.write(output_markdown)
+    return formatted_description
+
+
+def generate_validators_documentation(all_validators):
+    documentation = []
+    for validator in all_validators:
+        doc = {
+            "class_name": validator.__name__,
+            "name": validator.metadata.name,
+            "description": text_formatting(validator.metadata.description),
+        }
+        documentation.append(doc)
+    return documentation
+
+
+if __name__ == "__main__":
+    TEMPLATE_FILE = "formats_template.md.jinja"
+    OUTPUT_FILE = "./docs/formats.md"
+
+    documentation = generate_validators_documentation(all_validators)
+
+    template_loader = jinja2.FileSystemLoader(searchpath="./utils/")
+    template_env = jinja2.Environment(loader=template_loader, trim_blocks=True)
+    template = template_env.get_template(TEMPLATE_FILE)
+
+    output_markdown = template.render(validators=documentation)
+
+    with open(OUTPUT_FILE, "w") as f:
+        f.write(output_markdown)
