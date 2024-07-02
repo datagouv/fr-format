f7d418f41a23843c1b35653b5254390c4ca46c31
sarrabah <sarrabenarbia1996@gmail.com>
2024-07-02 12:29:17 +0200

    refactor: Reorganisation

--- src/frformat/common.py
+++ src/frformat/common.py
@@ -1,33 +1,26 @@
 import re
-
 from frformat.options import Options
 
 NBSP = "\u00A0"  # Non-breaking space
 NNBSP = "\u202F"  # Narrow non-breaking space
 
 
-def normalize_text(val: str, option_items: Options) -> str:
-    if option_items.ignore_case is True:
+def normalize_text(val: str, options: Options) -> str:
+    if options.ignore_case is True:
         val = val.lower()
-
-    if option_items.ignore_punctuation is True:
-        val = val.replace(",", " ")
-        val = val.replace("-", " ")
-
-    if option_items.ignore_underscore is True:
-        val = val.replace("_", " ")
-
-    if option_items.ignore_fullwidth_apostrophe is True:
-        val = val.replace("'", " ")
-
-    if option_items.ignore_space is True:
+    
+    if options.ignore_non_alphanumeric is True: 
+        val = re.sub(r"[^a-zA-Z0-9]", " ", val)
+    
+    if options.ignore_extra_white_space is True:
+        val = val.strip()
         val = val.replace("  ", " ")
-
-    if option_items.ignore_accents is True:
+    
+    if options.ignore_accents is True:
         val = re.sub(r"[èéêë]", "e", val)
         val = re.sub(r"[àáâãäå]", "a", val)
         val = re.sub(r"[ìíîï]", "i", val)
         val = re.sub(r"[òóôõö]", "o", val)
         val = re.sub(r"[ùúûü]", "u", val)
-
+    
     return val
