e8512730525589e2cddd8ace844adfa976efc647
sarrabah <sarrabenarbia1996@gmail.com>
2024-07-03 17:18:35 +0200

    fix: Change the order of comparaison instructions to avoid the space when we ignore the non alphanumeric characters

--- src/frformat/common.py
+++ src/frformat/common.py
@@ -10,18 +10,18 @@ def normalize_value(val: str, options: Options) -> str:
     if options.ignore_case is True:
         val = val.lower()
 
-    if options.ignore_non_alphanumeric is True:
-        val = re.sub(r"[^a-zA-Z0-9]", " ", val)
-
-    if options.ignore_extra_white_space is True:
-        val = val.strip()
-        val = val.replace("  ", " ")
-
     if options.ignore_accents is True:
         val = re.sub(r"[èéêë]", "e", val)
         val = re.sub(r"[àáâãäå]", "a", val)
         val = re.sub(r"[ìíîï]", "i", val)
         val = re.sub(r"[òóôõö]", "o", val)
         val = re.sub(r"[ùúûü]", "u", val)
+
+    if options.ignore_non_alphanumeric is True:
+        val = re.sub(r"[^a-zA-Z0-9]", " ", val)
+
+    if options.ignore_extra_white_space is True:
+        val = val.strip()
+        val = val.replace("  ", " ")
 
     return val
