303f32544a1841649f7fa712d92ab7131e2e92d6
sarrabah <sarrabenarbia1996@gmail.com>
2024-07-04 16:36:30 +0200

    docs: Add a detailed docstring to the Options class for better explanation

--- src/frformat/options.py
+++ src/frformat/options.py
@@ -3,8 +3,35 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Options:
+    """
+       The class Options is used to represent a list of necessary options to validate a French format.
+
+    Attributes:
+        ignore_case: Boolean
+            Indicates if case should be ignored.
+            When set to True, all characters in the string will be converted to lowercase.
+
+        ignore_accents: Boolean
+            Indicates if accents should be ignored.
+            When set to True, characters with accents will be replaced with their non-accented counterparts.
+            Example: 'à' will be replaced by 'a'.
+
+        ignore_non_alphanumeric: Boolean
+            Indicates if non-alphanumeric characters should be ignored.
+            When set to True, punctuation marks and symbols will be replaced by a space.
+
+        ignore_extra_white_space: Boolean
+            Indicates if extra white space should be ignored.
+            When set to True, multiple consecutive spaces will be treated as a single space, and leading or trailing spaces will be removed.
+
+        extra_valid_values: set of string
+            A collection of additional valid values.
+            This set includes any extra values that should be considered valid during the validation process, beyond the original set of valid values.
+            This allows for customized validation rules to accommodate special cases or exceptions.
+    """
+
     ignore_case: bool = False
+    ignore_accents: bool = False
     ignore_non_alphanumeric: bool = False
     ignore_extra_white_space: bool = False
-    ignore_accents: bool = False
     extra_valid_values: set = field(default_factory=set)
