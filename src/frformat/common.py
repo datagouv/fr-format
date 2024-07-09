97137e6d6e713387d88407537bad67461e423e0a
sarrabah <sarrabenarbia1996@gmail.com>
2024-07-09 16:54:23 +0200

    refactor: Using regex to replace extra whitespaces by one space

--- src/frformat/common.py
+++ src/frformat/common.py
@@ -18,8 +18,7 @@ def normalize_value(val: str, options: Options) -> str:
 
     if options.ignore_extra_white_space is True:
         val = val.strip()
-        val = val.replace("  ", " ")
-
+        val = re.sub(r"[\s\xa0]+", " ", val)
     return val
 
 
