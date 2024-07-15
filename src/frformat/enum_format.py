0c704ee6b2379ee2d76ce70d0e9d9c6152b524f9
sarrabah <sarrabenarbia1996@gmail.com>
2024-07-15 17:11:49 +0200

    refactor: Using private attributes

--- src/frformat/enum_format.py
+++ src/frformat/enum_format.py
@@ -12,21 +12,20 @@ def new(class_name: str, name: str, description: str, enum: Set[str]) -> Type:
         May preprocess the input and valid values according to given "options"."""
 
         def __init__(self, options: Options):
-            self.options = options
-            self.normalized_enum = {
-                normalize_value(e, self.options) for e in enum
-            }.union(
-                {
-                    normalize_value(e, self.options)
-                    for e in self.options.extra_valid_values
-                }
-            )
+            self._options = options
+            self._normalized_extra_values = {
+                normalize_value(e, self._options)
+                for e in self._options.extra_valid_values
+            }
+            self._normalized_enum = {
+                normalize_value(e, self._options) for e in enum
+            }.union(self._normalized_extra_values)
 
         metadata = Metadata(name, description)
 
         def is_valid(self, value: str) -> bool:
-            normalized_value = normalize_value(value, self.options)
-            return normalized_value in self.normalized_enum
+            normalized_value = normalize_value(value, self._options)
+            return normalized_value in self._normalized_enum
 
     EnumFormat.__name__ = class_name
     EnumFormat.__qualname__ = class_name
