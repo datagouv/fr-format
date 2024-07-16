b28f4da49e161caba1ac65aad5d33835b0846963
sarrabah <sarrabenarbia1996@gmail.com>
2024-07-16 12:24:43 +0200

    refactor: Transform a class method to an instance method

--- src/frformat/nomenclature_acte_format.py
+++ src/frformat/nomenclature_acte_format.py
@@ -58,22 +58,20 @@ class NomenclatureActe(CustomStrFormat):
         # Nomenclature reconnue et pas d'espace après l'oblique
         return "/ " not in value and nomenc in AUTHORIZED_VALUES
 
-    @classmethod
     def is_valid_with_details(
-        cls, value: str
+        self, value: str
     ) -> Union[ValidWithoutDetails, InvalidWithDetails]:
         """Check the validity, and return details if value is invalid"""
 
         details = []
-        instance = cls(Options())
-        if instance.is_valid(value):
+        if self.is_valid(value):
             return (True, None)
 
         if "/" not in value:
             details.append(MISSING_SLASH)
 
         else:
-            nomenc = cls._nomenclature(value)
+            nomenc = self._nomenclature(value)
 
             if nomenc.rstrip() in AUTHORIZED_VALUES or "/ " in value:
                 details.append(EXTRA_SPACE)
