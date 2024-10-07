6864e35f38f6827639c2653964c44767c5807d01
sarrabah <sarrabenarbia1996@gmail.com>
2024-10-07 17:58:17 +0200

    recator: Renaming

--- src/frformat/geo/code_region.py
+++ src/frformat/geo/code_region.py
@@ -29,7 +29,7 @@ description = (
     "Vérifie qu'il s'agit d'un code région selon le code officiel géographique donné"
 )
 all_cog_version: Dict[str, Set[str]] = {
-    "CODE_REGION_SET_COG_2024": CODE_REGION_SET_COG_2024,
-    "CODE_REGION_SET_COG_2023": CODE_REGION_SET_COG_2023,
+    "COG_2024": CODE_REGION_SET_COG_2024,
+    "COG_2023": CODE_REGION_SET_COG_2023,
 }
 CodeRegion = enum_format.new("CodeRegion", name, description, all_cog_version)
