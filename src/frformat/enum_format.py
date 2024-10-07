fa20fbb307cd91a61cbd8bc1b4538f50ea8428d4
sarrabah <sarrabenarbia1996@gmail.com>
2024-10-07 14:22:35 +0200

    refacto: Renaming

--- src/frformat/enum_format.py
+++ src/frformat/enum_format.py
@@ -1,11 +1,10 @@
-from typing import Set, Type
+from typing import Dict, Set, Type
 
 from frformat import CustomStrFormat, Metadata
 from frformat.common import normalize_value
 from frformat.options import Options
 
-
-def new(class_name: str, name: str, description: str, enum: Set[str]) -> Type:
+def new(class_name: str, name: str, description: str, geographical_enum: Dict[str, Set[str]]) -> Type:
     class EnumFormat(CustomStrFormat):
         """Checks if a value is in a given list
 
@@ -19,15 +18,15 @@ def new(class_name: str, name: str, description: str, enum: Set[str]) -> Type:
                 for e in self._options.extra_valid_values
             }
 
-            self._normalized_enum = {
-                normalize_value(e, self._options) for e in enum
+            self._normalized_geo_enum_value = {
+                normalize_value(e, self._options) for e in geographical_enum
             }.union(_normalized_extra_values)
 
         metadata = Metadata(name, description)
 
         def is_valid(self, value: str) -> bool:
             normalized_value = normalize_value(value, self._options)
-            return normalized_value in self._normalized_enum
+            return normalized_value in self._normalized_geo_enum_value
 
     EnumFormat.__name__ = class_name
     EnumFormat.__qualname__ = class_name
