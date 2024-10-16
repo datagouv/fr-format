71d4e3a82e4bb76f7c30427113abb44096f6361b
sarrabah <sarrabenarbia1996@gmail.com>
2024-10-16 17:34:30 +0200

    test: Ignore the lenient cases

--- src/tests/test_new_geo_fr.py
+++ src/tests/test_new_geo_fr.py
@@ -1,26 +1,35 @@
 from frformat import Canton, CodeRegion, Commune, Region
 from frformat.geo_enum_format import Millesime
-from tests.geo_testing import strict_lenient_test_helper_factory
 
 
 def test_code_region():
-    _test_code_region = strict_lenient_test_helper_factory(CodeRegion)
+    code_region_2023 = CodeRegion(Millesime.A2023)
+    code_region_2024 = CodeRegion(Millesime.A2024)
 
-    code_region_strict = ["01", "75"]
-    code_region_lenient = []
-    code_region_invalid = ["AA", "00"]
+    valid_test_cases = ["01", "75"]
+    invalid_test_cases = ["AA", "00"]
+    for tc in valid_test_cases:
+        assert code_region_2023.is_valid(tc)
+        assert code_region_2024.is_valid(tc)
 
-    _test_code_region(code_region_strict, code_region_lenient, code_region_invalid)
+    for tc in invalid_test_cases:
+        assert not code_region_2023.is_valid(tc)
+        assert not code_region_2024.is_valid(tc)
 
 
 def test_region():
-    _test_region = strict_lenient_test_helper_factory(Region)
+    region_2023 = Region(Millesime.A2023)
+    region_2024 = Region(Millesime.A2024)
 
-    region_strict = ["Centre-Val de Loire", "La Réunion", "Corse"]
-    region_lenient = ["Centre Val de Loire", "La Reunion", "corse"]
-    region_invalid = ["Beleriand", "Canyon Cosmo"]
+    valid_test_cases = ["Centre-Val de Loire", "La Réunion", "Corse"]
+    invalid_test_cases = ["Beleriand", "Canyon Cosmo"]
+    for tc in valid_test_cases:
+        assert region_2023.is_valid(tc)
+        assert region_2024.is_valid(tc)
 
-    _test_region(region_strict, region_lenient, region_invalid)
+    for tc in invalid_test_cases:
+        assert not region_2023.is_valid(tc)
+        assert not region_2024.is_valid(tc)
 
 
 def test_commune():
