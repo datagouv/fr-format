6033682f3fdd11af0f7a43e69e27861e86c92953
sarrabah <sarrabenarbia1996@gmail.com>
2024-10-16 14:07:56 +0200

    Refactor: Renaming

--- src/frformat/geo/region.py
+++ src/frformat/geo/region.py
@@ -7,8 +7,8 @@ from frformat.geo_enum_format import Millesime
 name = "Nom de région"
 description = "Vérifie les régions françaises valides"
 
-all_cog_version: Dict[Millesime, Set[str]] = {
+all_cog_versions: Dict[Millesime, Set[str]] = {
     Millesime.A2023: REGION_SET_COG_2023,
     Millesime.A2024: REGION_SET_COG_2024,
 }
-Region = geo_enum_format.new("Region", name, description, all_cog_version)
+Region = geo_enum_format.new("Region", name, description, all_cog_versions)
