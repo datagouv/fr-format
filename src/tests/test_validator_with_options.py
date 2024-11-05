2918e53a53bb25081a8f884ce36e89251cd07213
sarrabah <sarrabenarbia1996@gmail.com>
2024-11-05 11:23:34 +0100

    refactor: Ignore to use private variables

--- src/tests/test_validator_with_options.py
+++ src/tests/test_validator_with_options.py
@@ -5,15 +5,15 @@ from frformat.options import Options
 
 
 def test_validator():
-    _valid_values_enum: FrozenSet[str] = frozenset(
+    valid_values_enum: FrozenSet[str] = frozenset(
         {"Bonjour", "Réunion", "Plane!", "Fly   ", "La liste"}
     )
 
-    _Validator = enum_format.new(
-        "Validator", "validator", "Test validator", _valid_values_enum
+    Validator = enum_format.new(
+        "Validator", "validator", "Test validator", valid_values_enum
     )
 
-    _test_cases: List[Dict[str, Union[Options, List[str]]]] = [
+    test_cases: List[Dict[str, Union[Options, List[str]]]] = [
         {
             "options": Options(ignore_case=True),
             "valid_cases": ["bonjour"],
@@ -73,17 +73,17 @@ def test_validator():
         },
     ]
 
-    for tc in _test_cases:
+    for tc in test_cases:
         invalid_c = tc["invalid_cases"]
         valid_c = tc["valid_cases"]
 
         if isinstance(valid_c, List) and isinstance(invalid_c, List):
             for valid_ele in valid_c:
-                assert _Validator(tc["options"]).is_valid(
+                assert Validator(tc["options"]).is_valid(
                     valid_ele
                 ), f"Check that {valid_ele} is not valid"
 
             for invalid_ele in invalid_c:
-                assert not _Validator(tc["options"]).is_valid(
+                assert not Validator(tc["options"]).is_valid(
                     invalid_ele
                 ), f"Check that {invalid_ele} is valid"
