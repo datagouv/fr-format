6e8c2fe080b8e4fee36b8a14e5320f28f6116b85
sarrabah <sarrabenarbia1996@gmail.com>
2024-11-21 17:43:23 +0100

    refactor: Use a built-in function

--- src/tests/test_latest.py
+++ src/tests/test_latest.py
@@ -2,11 +2,9 @@ from frformat.geo_enum_format import Millesime
 
 
 def test_is_latest():
-    last_millesime_name = ""
-    for member in Millesime:
-        if member.name > last_millesime_name:
-            last_millesime_name = member.name
+    recent_millesime_name = max((member.name for member in Millesime))
+    recent_millesime = Millesime[recent_millesime_name]
 
-    last_millesime = Millesime[last_millesime_name]
-
-    assert Millesime.LATEST == last_millesime, "LATEST is not the most recent Millesime"
+    assert (
+        Millesime.LATEST == recent_millesime
+    ), "LATEST is not the most recent Millesime"
