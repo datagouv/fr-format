85003e7b349610d1f22863cb4d9f4372f5fd51e3
Ben Arbia Sarra <123973393+Sarrabah@users.noreply.github.com>
2024-11-25 11:54:43 +0100

    Test: Ensure LATEST Millesime is assigned to the most recent Millesime (#27)
    # Goal 
    * The purpose of this PR is to ensure that `LATEST` in the Millesime
    enum is correctly assigned to the most recent available Millesime.
    
    ---------
    
    Co-authored-by: Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>

---
+++ src/tests/test_latest.py
@@ -0,0 +1,10 @@
+from frformat import Millesime
+
+
+def test_is_latest():
+    recent_millesime_name = max(member.name for member in Millesime)
+    recent_millesime = Millesime[recent_millesime_name]
+
+    assert (
+        Millesime.LATEST == recent_millesime
+    ), "LATEST is not the most recent available Millesime"
