a5e59290ea6ff18e71a17dda97b32c649a967d1c
sarrabah <sarrabenarbia1996@gmail.com>
2024-11-25 11:47:49 +0100

    refactor: Remove the additional parentheses

--- src/tests/test_latest.py
+++ src/tests/test_latest.py
@@ -2,7 +2,7 @@ from frformat.geo_enum_format import Millesime
 
 
 def test_is_latest():
-    recent_millesime_name = max((member.name for member in Millesime))
+    recent_millesime_name = max(member.name for member in Millesime)
     recent_millesime = Millesime[recent_millesime_name]
 
     assert (
