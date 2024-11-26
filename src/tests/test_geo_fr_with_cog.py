e6edfcf1c11da7f2fbcc6fb72266c502ed64a2bb
sarrabah <sarrabenarbia1996@gmail.com>
2024-11-26 12:38:51 +0100

    refactor: Remove the nested dictionnary

--- src/tests/test_geo_fr_with_cog.py
+++ src/tests/test_geo_fr_with_cog.py
@@ -14,17 +14,18 @@ from frformat import (
 
 
 class ValidatorTest:
-    def __init__(self, cog, testCases, formatClass):
+    def __init__(self, cog, validTestCases, invalidTestCases, formatClass):
         self.cog = cog
-        self.testCases = testCases
+        self.validTestCases = validTestCases
+        self.invalidTestCases = invalidTestCases
         self.formatClass = formatClass
 
     def test_valid_cases(self):
-        for tc in self.testCases.get("valid", []):
+        for tc in self.validTestCases:
             assert self.formatClass(self.cog).is_valid(tc)
 
     def test_invalid_cases(self):
-        for tc in self.testCases.get("invalid", []):
+        for tc in self.invalidTestCases:
             assert not self.formatClass(self.cog).is_valid(tc)
 
     def run_all_tests(self):
@@ -38,180 +39,166 @@ def test_all_validators_with_cog():
             "name": "CodeRegion",
             "cog": Millesime.M2023,
             "formatClass": CodeRegion,
-            "test_cases": {"valid": ["01", "75"], "invalid": ["AA", "00", "7 5"]},
+            "validTestCases": ["01", "75"],
+            "invalidTestCases": ["AA", "00", "7 5"],
         },
         {
             "name": "CodeRegion",
             "cog": Millesime.M2024,
             "formatClass": CodeRegion,
-            "test_cases": {"valid": ["01", "75"], "invalid": ["AA", "00", "7 5"]},
+            "validTestCases": ["01", "75"],
+            "invalidTestCases": ["AA", "00", "7 5"],
         },
         {
             "name": "Region",
             "cog": Millesime.M2023,
             "formatClass": Region,
-            "test_cases": {
-                "valid": ["Centre-Val de Loire", "La Réunion", "Corse"],
-                "invalid": [
-                    "Beleriand",
-                    "Canyon Cosmo",
-                    "corse",
-                    "Centre val de Loire",
-                    "la reunion",
-                ],
-            },
+            "validTestCases": ["Centre-Val de Loire", "La Réunion", "Corse"],
+            "invalidTestCases": [
+                "Beleriand",
+                "Canyon Cosmo",
+                "corse",
+                "Centre val de Loire",
+                "la reunion",
+            ],
         },
         {
             "name": "Region",
             "cog": Millesime.M2024,
             "formatClass": Region,
-            "test_cases": {
-                "valid": ["Centre-Val de Loire", "La Réunion", "Corse"],
-                "invalid": [
-                    "Beleriand",
-                    "Canyon Cosmo",
-                    "corse",
-                    "Centre val de Loire",
-                    "la reunion",
-                ],
-            },
+            "validTestCases": ["Centre-Val de Loire", "La Réunion", "Corse"],
+            "invalidTestCases": [
+                "Beleriand",
+                "Canyon Cosmo",
+                "corse",
+                "Centre val de Loire",
+                "la reunion",
+            ],
         },
         {
             "name": "Commune",
             "cog": Millesime.M2023,
             "formatClass": Commune,
-            "test_cases": {
-                "valid": [
-                    "La Chapelle-Achard",
-                    "Beaumont-les-Nonains",
-                    "La Moncelle",
-                    "Montestrucq",
-                ],
-                "invalid": [
-                    "Costa del Sol",
-                    "Val-d'Usiers",
-                    "La Chapelle-Fleurigné",
-                    "Oullins-Pierre-Bénite",
-                    "la chapelle Fleurigne",
-                    "Costa-del Sol",
-                ],
-            },
+            "validTestCases": [
+                "La Chapelle-Achard",
+                "Beaumont-les-Nonains",
+                "La Moncelle",
+                "Montestrucq",
+            ],
+            "invalidTestCases": [
+                "Costa del Sol",
+                "Val-d'Usiers",
+                "La Chapelle-Fleurigné",
+                "Oullins-Pierre-Bénite",
+                "la chapelle Fleurigne",
+                "Costa-del Sol",
+            ],
         },
         {
             "name": "Commune",
             "cog": Millesime.M2024,
             "formatClass": Commune,
-            "test_cases": {
-                "valid": [
-                    "Oullins-Pierre-Bénite",
-                    "Bellac",
-                    "Val-d'Usiers",
-                    "La Chapelle-Fleurigné",
-                ],
-                "invalid": [
-                    "Costa del Sol",
-                    "Montestrucq",
-                    "La Chapelle-Achard",
-                    "Senonville",
-                    "montestrucq",
-                    "La Chapelle     Achard",
-                ],
-            },
+            "validTestCases": [
+                "Oullins-Pierre-Bénite",
+                "Bellac",
+                "Val-d'Usiers",
+                "La Chapelle-Fleurigné",
+            ],
+            "invalidTestCases": [
+                "Costa del Sol",
+                "Montestrucq",
+                "La Chapelle-Achard",
+                "Senonville",
+                "montestrucq",
+                "La Chapelle     Achard",
+            ],
         },
         {
             "name": "Canton",
             "cog": Millesime.M2023,
             "formatClass": Canton,
-            "test_cases": {
-                "valid": [
-                    "Lagnieu",
-                    "Meximieux",
-                ],
-                "invalid": ["Paris", "Lyon", "paris"],
-            },
+            "validTestCases": [
+                "Lagnieu",
+                "Meximieux",
+            ],
+            "invalidTestCases": ["Paris", "Lyon", "paris"],
         },
         {
             "name": "Canton",
             "cog": Millesime.M2024,
             "formatClass": Canton,
-            "test_cases": {
-                "valid": ["Paris", "Lyon"],
-                "invalid": ["Saint Quentin", "saint  quentin"],
-            },
+            "validTestCases": ["Paris", "Lyon"],
+            "invalidTestCases": ["Saint Quentin", "saint  quentin"],
         },
         {
             "name": "CodePaysISO2",
             "cog": Millesime.M2023,
             "formatClass": CodePaysISO2,
-            "test_cases": {"valid": ["BV", "SJ"], "invalid": ["RWA", "TCD", "rwa"]},
+            "validTestCases": ["BV", "SJ"],
+            "invalidTestCases": ["RWA", "TCD", "rwa"],
         },
         {
             "name": "CodePaysISO2",
             "cog": Millesime.M2024,
             "formatClass": CodePaysISO2,
-            "test_cases": {"valid": ["FR", "JP"], "invalid": ["BV", "SJ", "bv"]},
+            "validTestCases": ["FR", "JP"],
+            "invalidTestCases": ["BV", "SJ", "bv"],
         },
         {
             "name": "CodePaysISO3",
             "cog": Millesime.M2023,
             "formatClass": CodePaysISO3,
-            "test_cases": {"valid": ["BVT", "SJM"], "invalid": ["BF", "GH", "gh"]},
+            "validTestCases": ["BVT", "SJM"],
+            "invalidTestCases": ["BF", "GH", "gh"],
         },
         {
             "name": "CodePaysISO3",
             "cog": Millesime.M2024,
             "formatClass": CodePaysISO3,
-            "test_cases": {"valid": ["FRA", "JPN"], "invalid": ["BVT", "SJM", "bvt"]},
+            "validTestCases": ["FRA", "JPN"],
+            "invalidTestCases": ["BVT", "SJM", "bvt"],
         },
         {
             "name": "Departement",
             "cog": Millesime.M2023,
             "formatClass": Departement,
-            "test_cases": {
-                "valid": ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"],
-                "invalid": ["Charente-Inférieure", "Vendee", "Alpes  maritimes"],
-            },
+            "validTestCases": ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"],
+            "invalidTestCases": ["Charente-Inférieure", "Vendee", "Alpes  maritimes"],
         },
         {
             "name": "Departement",
             "cog": Millesime.M2024,
             "formatClass": Departement,
-            "test_cases": {
-                "valid": ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"],
-                "invalid": ["Charente-Inférieure", "Vendee", "Alpes  maritimes"],
-            },
+            "validTestCases": ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"],
+            "invalidTestCases": ["Charente-Inférieure", "Vendee", "Alpes  maritimes"],
         },
         {
             "name": "Pays",
             "cog": Millesime.M2024,
             "formatClass": Pays,
-            "test_cases": {
-                "valid": ["France", "Pays-Bas", "Bosnie-Herzégovine"],
-                "invalid": ["L'Eldorado", "Zubrowska", "Pays Bas", "france"],
-            },
+            "validTestCases": ["France", "Pays-Bas", "Bosnie-Herzégovine"],
+            "invalidTestCases": ["L'Eldorado", "Zubrowska", "Pays Bas", "france"],
         },
         {
             "name": "NumeroDepartement",
             "cog": Millesime.M2023,
             "formatClass": NumeroDepartement,
-            "test_cases": {
-                "valid": ["05", "2B", "974"],
-                "invalid": ["99", "051", "2b", "  97 4"],
-            },
+            "validTestCases": ["05", "2B", "974"],
+            "invalidTestCases": ["99", "051", "2b", "  97 4"],
         },
         {
             "name": "NumeroDepartement",
             "cog": Millesime.M2024,
             "formatClass": NumeroDepartement,
-            "test_cases": {
-                "valid": ["05", "2B", "974"],
-                "invalid": ["99", "051", "2b", "  97 4"],
-            },
+            "validTestCases": ["05", "2B", "974"],
+            "invalidTestCases": ["99", "051", "2b", "  97 4"],
         },
     ]
 
     for tc in test_cases:
-        validatorTest = ValidatorTest(tc["cog"], tc["test_cases"], tc["formatClass"])
+        validatorTest = ValidatorTest(
+            tc["cog"], tc["validTestCases"], tc["invalidTestCases"], tc["formatClass"]
+        )
         validatorTest.run_all_tests()
 
 
