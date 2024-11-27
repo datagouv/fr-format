19d2b157b64a5245fba871655875a5b8e5de1a19
sarrabah <sarrabenarbia1996@gmail.com>
2024-11-27 13:06:46 +0100

    perf: Use a new const to have all the repeated values

--- src/frformat/geo/code_commune_insee.py
+++ src/frformat/geo/code_commune_insee.py
@@ -2,8 +2,9 @@ from typing import Dict, FrozenSet
 
 from frformat import geo_enum_format
 from frformat.geo.code_commune_insee_frozenset import (
-    CODES_COMMUNES_INSEE_COG_2023,
-    CODES_COMMUNES_INSEE_COG_2024,
+    INTERSECTION_CODE_COMMUNE_INSEE,
+    SPECIFIC_VALUES_CODE_COMMUNES_INSEE_COG_2023,
+    SPECIFIC_VALUES_CODE_COMMUNES_INSEE_COG_2024,
 )
 from frformat.geo_enum_format import Millesime
 
@@ -11,8 +12,9 @@ name = "Code commune INSEE"
 description = "Vérifie que le code commune correspond bien à un code commune INSEE pour un Code Officiel Géographique donné"
 
 all_cog_versions: Dict[Millesime, FrozenSet[str]] = {
-    Millesime.M2023: CODES_COMMUNES_INSEE_COG_2023,
-    Millesime.M2024: CODES_COMMUNES_INSEE_COG_2024,
+    Millesime.M2023: SPECIFIC_VALUES_CODE_COMMUNES_INSEE_COG_2023,
+    Millesime.M2024: SPECIFIC_VALUES_CODE_COMMUNES_INSEE_COG_2024,
+    Millesime.INTERSECTION: INTERSECTION_CODE_COMMUNE_INSEE,
 }
 CodeCommuneInsee = geo_enum_format.new(
     "CodeCommuneInsee", name, description, all_cog_versions
