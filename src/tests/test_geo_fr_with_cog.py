065b863186e179d974e627fa22c2ed8e7a81699d
Ben Arbia Sarra <123973393+Sarrabah@users.noreply.github.com>
2024-11-28 14:57:37 +0100

    Refactor: Refactor the test file for geographic formats with cog integration (#28)
    # Goal
    * The aim of this PR is to refactor and reorganize the test code for
    validating geographical french formats with a specific Official
    Geographic Code `cog`, to be more clear and comprehensive.

--- src/tests/test_geo_fr_with_cog.py
+++ src/tests/test_geo_fr_with_cog.py
@@ -13,226 +13,220 @@ from frformat import (
 )
 
 
-def test_code_region():
-    code_region_2023 = CodeRegion(Millesime.M2023)
-    code_region_2024 = CodeRegion(Millesime.M2024)
+class ValidatorTest:
+    def __init__(self, cog, validTestCases, invalidTestCases, formatClass):
+        self.cog = cog
+        self.validTestCases = validTestCases
+        self.invalidTestCases = invalidTestCases
+        self.formatClass = formatClass
 
-    valid_test_cases = ["01", "75"]
-    invalid_test_cases = ["AA", "00", "7 5"]
-    for tc in valid_test_cases:
-        assert code_region_2023.is_valid(tc)
-        assert code_region_2024.is_valid(tc)
+    def test_valid_cases(self):
+        for tc in self.validTestCases:
+            assert self.formatClass(self.cog).is_valid(
+                tc
+            ), f"Check that {tc} is not valid when the class format is {self.formatClass} and the cog is equal to {self.cog}"
 
-    for tc in invalid_test_cases:
-        assert not code_region_2023.is_valid(tc)
-        assert not code_region_2024.is_valid(tc)
+    def test_invalid_cases(self):
+        for tc in self.invalidTestCases:
+            assert not self.formatClass(self.cog).is_valid(
+                tc
+            ), f"Check that {tc} is valid when the class format is {self.formatClass} and the cog is equal to {self.cog}."
+
+    def run_all_tests(self):
+        self.test_valid_cases()
+        self.test_invalid_cases()
 
 
-def test_region():
-    region_2023 = Region(Millesime.M2023)
-    region_2024 = Region(Millesime.M2024)
-
-    valid_test_cases = ["Centre-Val de Loire", "La Réunion", "Corse"]
-    invalid_test_cases = [
-        "Beleriand",
-        "Canyon Cosmo",
-        "corse",
-        "Centre val de Loire",
-        "la reunion",
+def test_all_validators_with_cog():
+    test_cases = [
+        {
+            "name": "code_region_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": CodeRegion,
+            "validTestCases": ["01", "75"],
+            "invalidTestCases": ["AA", "00", "7 5"],
+        },
+        {
+            "name": "code_region_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": CodeRegion,
+            "validTestCases": ["01", "75"],
+            "invalidTestCases": ["AA", "00", "7 5"],
+        },
+        {
+            "name": "region_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": Region,
+            "validTestCases": ["Centre-Val de Loire", "La Réunion", "Corse"],
+            "invalidTestCases": [
+                "Beleriand",
+                "Canyon Cosmo",
+                "corse",
+                "Centre val de Loire",
+                "la reunion",
+            ],
+        },
+        {
+            "name": "region_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": Region,
+            "validTestCases": ["Centre-Val de Loire", "La Réunion", "Corse"],
+            "invalidTestCases": [
+                "Beleriand",
+                "Canyon Cosmo",
+                "corse",
+                "Centre val de Loire",
+                "la reunion",
+            ],
+        },
+        {
+            "name": "commune_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": Commune,
+            "validTestCases": [
+                "La Chapelle-Achard",
+                "Beaumont-les-Nonains",
+                "La Moncelle",
+                "Montestrucq",
+            ],
+            "invalidTestCases": [
+                "Costa del Sol",
+                "Val-d'Usiers",
+                "La Chapelle-Fleurigné",
+                "Oullins-Pierre-Bénite",
+                "la chapelle Fleurigne",
+                "Costa-del Sol",
+            ],
+        },
+        {
+            "name": "commune_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": Commune,
+            "validTestCases": [
+                "Oullins-Pierre-Bénite",
+                "Bellac",
+                "Val-d'Usiers",
+                "La Chapelle-Fleurigné",
+            ],
+            "invalidTestCases": [
+                "Costa del Sol",
+                "Montestrucq",
+                "La Chapelle-Achard",
+                "Senonville",
+                "montestrucq",
+                "La Chapelle     Achard",
+            ],
+        },
+        {
+            "name": "canton_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": Canton,
+            "validTestCases": [
+                "Lagnieu",
+                "Meximieux",
+            ],
+            "invalidTestCases": ["Paris", "Lyon", "paris"],
+        },
+        {
+            "name": "canton_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": Canton,
+            "validTestCases": ["Paris", "Lyon"],
+            "invalidTestCases": ["Saint Quentin", "saint  quentin"],
+        },
+        {
+            "name": "code_paysISO2_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": CodePaysISO2,
+            "validTestCases": ["BV", "SJ"],
+            "invalidTestCases": ["RWA", "TCD", "rwa"],
+        },
+        {
+            "name": "code_paysISO2_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": CodePaysISO2,
+            "validTestCases": ["FR", "JP"],
+            "invalidTestCases": ["BV", "SJ", "bv"],
+        },
+        {
+            "name": "code_paysISO3_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": CodePaysISO3,
+            "validTestCases": ["BVT", "SJM"],
+            "invalidTestCases": ["BF", "GH", "gh"],
+        },
+        {
+            "name": "code_paysISO3_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": CodePaysISO3,
+            "validTestCases": ["FRA", "JPN"],
+            "invalidTestCases": ["BVT", "SJM", "bvt"],
+        },
+        {
+            "name": "departement_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": Departement,
+            "validTestCases": ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"],
+            "invalidTestCases": ["Charente-Inférieure", "Vendee", "Alpes  maritimes"],
+        },
+        {
+            "name": "departement_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": Departement,
+            "validTestCases": ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"],
+            "invalidTestCases": ["Charente-Inférieure", "Vendee", "Alpes  maritimes"],
+        },
+        {
+            "name": "pays_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": Pays,
+            "validTestCases": ["France", "Pays-Bas", "Bosnie-Herzégovine"],
+            "invalidTestCases": ["L'Eldorado", "Zubrowska", "Pays Bas", "france"],
+        },
+        {
+            "name": "numero_departement_millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": NumeroDepartement,
+            "validTestCases": ["05", "2B", "974"],
+            "invalidTestCases": ["99", "051", "2b", "  97 4"],
+        },
+        {
+            "name": "numero_departement_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": NumeroDepartement,
+            "validTestCases": ["05", "2B", "974"],
+            "invalidTestCases": ["99", "051", "2b", "  97 4"],
+        },
+        {
+            "name": "code_commune_insee__millesime2023",
+            "cog": Millesime.M2023,
+            "formatClass": CodeCommuneInsee,
+            "validTestCases": ["01015", "2B002"],
+            "invalidTestCases": ["77777", "  01015"],
+        },
+        {
+            "name": "code_commune_insee_millesime2024",
+            "cog": Millesime.M2024,
+            "formatClass": CodeCommuneInsee,
+            "validTestCases": ["64300", "2A331"],
+            "invalidTestCases": ["64402", "64  300"],
+        },
     ]
-    for tc in valid_test_cases:
-        assert region_2023.is_valid(tc)
-        assert region_2024.is_valid(tc)
 
-    for tc in invalid_test_cases:
-        assert not region_2023.is_valid(tc)
-        assert not region_2024.is_valid(tc)
-
-
-def test_commune():
-    commune_2023 = Commune(Millesime.M2023)
-    commune_2024 = Commune(Millesime.M2024)
-
-    valid_test_cases_cog_2023 = [
-        "La Chapelle-Achard",
-        "Beaumont-les-Nonains",
-        "La Moncelle",
-        "Montestrucq",
-    ]
-    invalid_test_cases_cog_2023 = [
-        "Costa del Sol",
-        "Val-d'Usiers",
-        "La Chapelle-Fleurigné",
-        "Oullins-Pierre-Bénite",
-        "la chapelle Fleurigne",
-        "Costa-del Sol",
-    ]
-
-    valid_test_cases_cog_2024 = [
-        "Oullins-Pierre-Bénite",
-        "Bellac",
-        "Val-d'Usiers",
-        "La Chapelle-Fleurigné",
-    ]
-
-    invalid_test_cases_cog_2024 = [
-        "Costa del Sol",
-        "Montestrucq",
-        "La Chapelle-Achard",
-        "Senonville",
-        "montestrucq",
-        "La Chapelle     Achard",
-    ]
-
-    for tc in valid_test_cases_cog_2023:
-        assert commune_2023.is_valid(tc)
-
-    for tc in invalid_test_cases_cog_2023:
-        assert not commune_2023.is_valid(tc)
-
-    for tc in valid_test_cases_cog_2024:
-        assert commune_2024.is_valid(tc)
-
-    for tc in invalid_test_cases_cog_2024:
-        assert not commune_2024.is_valid(tc)
+    for tc in test_cases:
+        validatorTest = ValidatorTest(
+            tc["cog"], tc["validTestCases"], tc["invalidTestCases"], tc["formatClass"]
+        )
+        validatorTest.run_all_tests()
 
 
-def test_canton():
-    canton_2023 = Canton(Millesime.M2023)
-    canton_2024 = Canton(Millesime.M2024)
-
-    valid_test_cases_cog_2023 = [
-        "Lagnieu",
-        "Meximieux",
-    ]
-    invalid_test_cases_cog_2023 = ["Paris", "Lyon", "paris"]
-
-    valid_test_cases_cog_2024 = ["Paris", "Lyon"]
-
-    invalid_test_cases_cog_2024 = ["Saint Quentin", "saint  quentin"]
-
-    for tc in valid_test_cases_cog_2023:
-        assert canton_2023.is_valid(tc)
-
-    for tc in invalid_test_cases_cog_2023:
-        assert not canton_2023.is_valid(tc)
-
-    for tc in valid_test_cases_cog_2024:
-        assert canton_2024.is_valid(tc)
-
-    for tc in invalid_test_cases_cog_2024:
-        assert not canton_2024.is_valid(tc)
-
-
-def test_code_pays():
-    code_pays_2023_IS02 = CodePaysISO2(Millesime.M2023)
-    code_pays_2024_IS02 = CodePaysISO2(Millesime.M2024)
-
-    code_pays_2023_IS03 = CodePaysISO3(Millesime.M2023)
-    code_pays_2024_IS03 = CodePaysISO3(Millesime.M2024)
-
-    valid_test_cases_iso2_cog_2023 = ["BV", "SJ"]
-    invalid_test_cases_iso2_cog_2023 = ["RWA", "TCD", "rwa"]
-
-    valid_test_cases_iso2_cog_2024 = ["FR", "JP"]
-    invalid_test_cases_iso2_cog_2024 = ["BV", "SJ", "bv"]
-
-    valid_test_cases_iso3_cog_2023 = ["BVT", "SJM"]
-    invalid_test_cases_iso3_cog_2023 = ["BF", "GH", "gh"]
-
-    valid_test_cases_iso3_cog_2024 = ["FRA", "JPN"]
-    invalid_test_cases_iso3_cog_2024 = ["BVT", "SJM", "bvt"]
-
-    for tc in valid_test_cases_iso2_cog_2023:
-        assert code_pays_2023_IS02.is_valid(tc)
-
-    for tc in invalid_test_cases_iso2_cog_2023:
-        assert not code_pays_2023_IS02.is_valid(tc)
-
-    for tc in valid_test_cases_iso2_cog_2024:
-        assert code_pays_2024_IS02.is_valid(tc)
-
-    for tc in invalid_test_cases_iso2_cog_2024:
-        assert not code_pays_2024_IS02.is_valid(tc)
-
-    for tc in valid_test_cases_iso3_cog_2023:
-        assert code_pays_2023_IS03.is_valid(tc)
-
-    for tc in invalid_test_cases_iso3_cog_2023:
-        assert not code_pays_2023_IS03.is_valid(tc)
-
-    for tc in valid_test_cases_iso3_cog_2024:
-        assert code_pays_2024_IS03.is_valid(tc)
-
-    for tc in invalid_test_cases_iso3_cog_2024:
-        assert not code_pays_2024_IS03.is_valid(tc)
-
-
-def test_code_commune_insee():
+def test_code_commune_insee_format():
     code_commune_insee_cog_2023 = CodeCommuneInsee(Millesime.M2023)
     code_commune_insee_cog_2024 = CodeCommuneInsee(Millesime.M2024)
 
     cog_2023_value = "01015"
-    assert code_commune_insee_cog_2023.is_valid(cog_2023_value)
     assert code_commune_insee_cog_2023.format(cog_2023_value) == cog_2023_value
-    assert code_commune_insee_cog_2023.is_valid("2B002")
-
-    cog_2023_invalid_values = ["77777", "  01015"]
-    for iv in cog_2023_invalid_values:
-        assert not code_commune_insee_cog_2023.is_valid(iv)
 
     cog_2024_value = "64300"
 
-    assert code_commune_insee_cog_2024.is_valid(cog_2024_value)
     assert code_commune_insee_cog_2024.format(cog_2024_value) == cog_2024_value
-    assert code_commune_insee_cog_2024.is_valid("2A331")
-
-    cog_2024_invalid_values = ["64402", "64  300"]
-
-    for iv in cog_2024_invalid_values:
-        assert not code_commune_insee_cog_2024.is_valid(iv)
-
-
-def test_departement():
-    departement_cog_2023 = Departement(Millesime.M2023)
-    departement_cog_2024 = Departement(Millesime.M2024)
-
-    valid_test_cases = ["Alpes-Maritimes", "Gard", "Mayotte", "Vendée"]
-    invalid_test_cases = ["Charente-Inférieure", "Vendee", "Alpes  maritimes"]
-
-    for tc in valid_test_cases:
-        assert departement_cog_2023.is_valid(tc)
-        assert departement_cog_2024.is_valid(tc)
-
-    for tc in invalid_test_cases:
-        assert not departement_cog_2023.is_valid(tc)
-        assert not departement_cog_2024.is_valid(tc)
-
-
-def test_pays():
-    pays_cog_2024 = Pays(Millesime.M2024)
-
-    valid_pays_cog_2024 = ["France", "Pays-Bas", "Bosnie-Herzégovine"]
-    invalid_pays_cog_2024 = ["L'Eldorado", "Zubrowska", "Pays Bas", "france"]
-
-    for tc in valid_pays_cog_2024:
-        assert pays_cog_2024.is_valid(tc)
-
-    for tc in invalid_pays_cog_2024:
-        assert not pays_cog_2024.is_valid(tc)
-
-
-def test_numero_departement():
-    num_departement_cog_2023 = NumeroDepartement(Millesime.M2023)
-    num_departement_cog_2024 = NumeroDepartement(Millesime.M2024)
-
-    num_departement_valid = ["05", "2B", "974"]
-    num_departement_invalid = ["99", "051", "2b", "  97 4"]
-
-    for tc in num_departement_valid:
-        assert num_departement_cog_2023.is_valid(tc)
-        assert num_departement_cog_2024.is_valid(tc)
-
-    for tc in num_departement_invalid:
-        assert not num_departement_cog_2023.is_valid(tc)
-        assert not num_departement_cog_2024.is_valid(tc)
