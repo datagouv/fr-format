58b563ad7efebb3a7e38a06cc94feb677af66539
sarrabah <sarrabenarbia1996@gmail.com>
2024-12-09 13:58:24 +0100

    feat: Sort versions

--- src/frformat/utils/versioned_set.py
+++ src/frformat/utils/versioned_set.py
@@ -1,16 +1,64 @@
 from dataclasses import dataclass
-from typing import Dict, FrozenSet, Generic, List, TypeVar, Tuple
+from typing import (
+    Dict,
+    FrozenSet,
+    Generic,
+    List,
+    Protocol,
+    Tuple,
+    Type,
+    TypeVar,
+    runtime_checkable,
+)
 
 Data = FrozenSet
 
 V = TypeVar("V", bound="Version")
 
 
+class Version(Protocol):
+    def get_id(self) -> str:
+        ...
+
+    @classmethod
+    def is_sorted(cls) -> bool:
+        """If a version class declares itself sorted, it should be sortable by implementing all six
+        following comparison operators `__lt__()`, `__le__()`, `__gt__()`, `__ge__()`, `__eq__()` and `__neq__()`.
+        See the `functools.total_ordering` decorator helper documentation.
+        """
+        return False
+
+
+@runtime_checkable
+class _SortableVersion(Version, Protocol):
+    """A version subclass that is sortable
+    For type checking purposes only"""
+
+    def __lt__(self, v) -> bool:
+        ...
+
+    def __le__(self, v) -> bool:
+        ...
+
+    def __gt__(self, v) -> bool:
+        ...
+
+    def __ge__(self, v) -> bool:
+        ...
+
+    def __eq__(self, v) -> bool:
+        ...
+
+    def __neq__(self, v) -> bool:
+        ...
+
+
 @dataclass(frozen=True, order=True)
-class Version:
+class BaseVersion(Version):
     id: str
 
-    # Functions to be sorted, with default sorting by id
+    def get_id(self) -> str:
+        return self.id
 
 
 class VersionedSet(Generic[V]):
@@ -23,23 +71,27 @@ class VersionedSet(Generic[V]):
     version = "latest" ? What behavior ?
     """
 
+    _version_class: Type[V]
+
     def __init__(self):
-        self._version: Dict[str, Tuple[V, Data]] = {}
+        self._versionned_data: Dict[str, Tuple[V, Data]] = {}
 
     def ls(self) -> List[V]:
         """List all available versions"""
-        return sorted(version for version, _ in self._version.values())
+        return [
+            self._versionned_data[id][0] for id in sorted(self._versionned_data.keys())
+        ]
 
     def add_version(self, new_version: V, data: Data):
-        """ Adds a version of data
+        """Adds a version of data
 
         Adding a version with already existing ID results in a ValueError.
 
         """
-        if new_version.id in self._version.keys():
-            raise ValueError(f"The version id {new_version.id} already exists!")
+        if new_version.get_id() in self._versionned_data.keys():
+            raise ValueError(f"The version id {new_version.get_id()} already exists!")
 
-        self._version.update({new_version.id: (new_version, data)})
+        self._versionned_data.update({new_version.get_id(): (new_version, data)})
 
     def get_data(self, version_id: str) -> Data | None:
         """
@@ -51,18 +103,22 @@ class VersionedSet(Generic[V]):
         with the version having the highest id.
         """
 
-        if version_id == "latest":
-            latest_version = max(self.ls())
-            _, data = self._version[latest_version.id]
+        if self._version_class.is_sorted() and version_id == "latest":
+            version_list = self.ls()
+            assert (
+                version_list is List[_SortableVersion]
+            ), "Version class with `is_sorted() == True` shoud be sortable (see documentation)"
+            latest_version = max(version_list)
+            _, data = self._versionned_data[latest_version.id]
             return data
-        
-        version_data = self._version.get(version_id)
-        return version_data[1] if version_data else None
+
+        data_with_version = self._versionned_data.get(version_id)
+        return data_with_version[1] if data_with_version else None
 
 
-vs = VersionedSet()
-vs.add_version(Version("2025"), frozenset({"coucou"}))  # ok
-vs.add_version(Version("2024"), frozenset({"one"}))  # ok
+vs = VersionedSet[BaseVersion]()
+vs.add_version(BaseVersion("2025"), frozenset({"coucou"}))  # ok
+vs.add_version(BaseVersion("2024"), frozenset({"one"}))  # ok
 # vs.add_version(Version("2024"), frozenset({"two"}))  # not ok
 
 print(vs.ls())
