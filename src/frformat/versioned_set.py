f399f47d61c63cc817d35daffa307fa88da88ff7
sarrabah <sarrabenarbia1996@gmail.com>
2024-12-10 10:55:48 +0100

    refactor: Remove comments

--- src/frformat/versioned_set.py
+++ src/frformat/versioned_set.py
@@ -68,7 +68,6 @@ class VersionedSet(Generic[V]):
 
     Version IDs should be unique.
 
-    version = "latest" ? What behavior ?
     """
 
     _version_class: Type[V]
@@ -114,55 +113,3 @@ class VersionedSet(Generic[V]):
 
         data_with_version = self._versionned_data.get(version_id)
         return data_with_version[1] if data_with_version else None
-
-
-vs = VersionedSet[BaseVersion]()
-vs.add_version(BaseVersion("2025"), frozenset({"coucou"}))  # ok
-vs.add_version(BaseVersion("2024"), frozenset({"one"}))  # ok
-# vs.add_version(Version("2024"), frozenset({"two"}))  # not ok
-
-print(vs.ls())
-
-vs_getted = vs.get_data("2025")  # ok
-print("returned data: ", vs_getted)
-vs.get_data("204")  # not ok
-
-
-"""
-OPEN
-J'ai un Versionned set, comment je sais quelle 'version_id' je peux utiliser
-// leur format
-
-VERSION_ID == STRING ??
-
-Le problème de version_id = str, c'est que en tant qu'utilisateur d'un
-VersionedSet, soit j'ai la charge mentale des versions autorisées, soit je
-suis obligé de regarde l'implémentation (et le but de l'abstraction c'est
-d'éviter ça)
-
-On a plusieurs jeux de données qui ont les mêmes ID de version et la chaîne de
-caractère ne permet pas de la garantir.
-
-Et en même temps, on peut imaginer des disponibilités partielles pour les
-mêmes ID de version (par exemple: certaines données sont dispos pour des COG
-depuis 1990, d'autres seulement depuis 2000)
-
-VERSION_ID == ENUM ??
-
-- Est-ce que c'est simple / intuitif de restreindre un enum à seulement
-modalités ?
-    - Si oui, ça demande de changer le type à chaque fois qu'on modifie les
-      données -> c'est un peu nul
-    - Si non -> dans ce cas il faut gérer la situation ou les données ne sont
-      pas disponibles. Est-ce que c'est grave :
-        - un peu parce que ça donne l'illusion que les données sont
-          disponibles
-        - le typage n'intercepte pas statiquement une erreur
-
-DONE
-
-Should Data be a Set -> Frozenset
-
-Est-ce que c'est dans le périmètre de cette classe de gérer LATEST ?
-
-"""
