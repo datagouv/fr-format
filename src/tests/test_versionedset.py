2e047706bdf5ed6fb2b50f53d833703ea9f624b7
sarrabah <sarrabenarbia1996@gmail.com>
2024-12-16 16:25:44 +0100

    refactor: Represent BasVersion class inside the test

--- src/tests/test_versionedset.py
+++ src/tests/test_versionedset.py
@@ -1,8 +1,45 @@
+from functools import total_ordering
+
 import pytest
 
-from frformat.versioned_set import BaseVersion, VersionedSet
+from frformat.versioned_set import Version, VersionedSet
 
 
 def test_versionedset():
+    @total_ordering
+    class BaseVersion(Version):
+        def __init__(self, id: str) -> None:
+            self.id = id
+
+        def get_id(self) -> str:
+            return self.id
+
+        @classmethod
+        def is_sorted(cls) -> bool:
+            return True
+
+        def __eq__(self, other):
+            return self.id == other.id
+
+        def __lt__(self, other):
+            return self.id < other.id
+    
     vs = VersionedSet[BaseVersion]()
-    assert vs.get_data("latest") is None
+
+    with pytest.raises(ValueError):
+        vs.get_data("latest")
+
+    assert vs.add_version(BaseVersion("2025"), frozenset({"Paris"})) is None
+    assert vs.add_version(BaseVersion("2024"), frozenset({"Lyon"})) is None
+
+    with pytest.raises(ValueError):
+        vs.add_version(BaseVersion("2024"), frozenset({"Marseille"}))
+
+    assert vs.ls() == [BaseVersion("2024"), BaseVersion("2025")]
+    assert not vs.ls() == [BaseVersion("2025"), BaseVersion("2024")]
+
+    assert vs.get_data("2025") == frozenset({"Paris"})
+    assert vs.get_data("2021") is None
+
+    assert vs.get_data("latest") == frozenset({"Paris"})
+    assert not vs.get_data("latest") == frozenset({"Lyon"})
