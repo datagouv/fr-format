c250a973a737f4bc8f61b95b4de2eb1ed494de1f
sarrabah <sarrabenarbia1996@gmail.com>
2024-12-18 11:06:07 +0100

    feat: Implement versioned set inside geo enum format

--- src/frformat/geo_enum_format_with_versionedset.py
+++ src/frformat/geo_enum_format_with_versionedset.py
@@ -1,11 +1,11 @@
 from enum import Enum, auto
 from functools import total_ordering
-from typing import Dict, FrozenSet, Type
+from typing import Type
 
 from frformat import CustomStrFormat, Metadata
 from frformat.common import normalize_value
 from frformat.options import Options
-from frformat.versioned_set import Version
+from frformat.versioned_set import Version, VersionedSet
 
 
 @total_ordering
@@ -31,9 +31,9 @@ def new(
     class_name: str,
     name: str,
     description: str,
-    geographical_enums: Dict[Millesime, FrozenSet[str]],
+    versionned_geographical_enums: VersionedSet,
 ) -> Type:
-    class GeoEnumFormat(CustomStrFormat):
+    class GeoEnumFormatVersionned(CustomStrFormat):
         """Checks if a value is in a given geographical referential, with validation for the vintage of choice
 
         Geographical data in France is revised once a year, with new valid values set given by the "Code Officiel Géographique" (cog).
@@ -47,16 +47,18 @@ def new(
                 for e in self._options.extra_valid_values
             }
 
-            if cog not in geographical_enums.keys():
+            if cog not in versionned_geographical_enums.ls():
                 raise ValueError(
                     f"No data available for official geographical code (cog): {cog.name}"
                 )
 
-            _valid_values = geographical_enums[cog]
-
-            self._normalized_geo_enum_value = {
-                normalize_value(val, self._options) for val in _valid_values
-            }.union(_normalized_extra_values)
+            _valid_values = versionned_geographical_enums.get_data(cog.get_id())
+            if _valid_values is not None:
+                self._normalized_geo_enum_value = {
+                    normalize_value(val, self._options) for val in _valid_values
+                }.union(_normalized_extra_values)
+            else:
+                raise ValueError("There is No valid values! ")
 
         metadata = Metadata(name, description)
 
@@ -64,7 +66,7 @@ def new(
             normalized_value = normalize_value(value, self._options)
             return normalized_value in self._normalized_geo_enum_value
 
-    GeoEnumFormat.__name__ = class_name
-    GeoEnumFormat.__qualname__ = class_name
+    GeoEnumFormatVersionned.__name__ = class_name
+    GeoEnumFormatVersionned.__qualname__ = class_name
 
-    return GeoEnumFormat
+    return GeoEnumFormatVersionned
---
+++ src/tests/test_geo_with_versionedset.py
@@ -0,0 +1,33 @@
+import pytest
+
+from frformat.geo_enum_format_with_versionedset import Millesime, new
+from frformat.versioned_set import VersionedSet
+
+
+def test_geo_enum_format_versionned():
+    versioned_data = VersionedSet()
+    versioned_data.add_version(
+        Millesime.M2023,
+        frozenset({"Ambléon", "Ambronay", "Ambutrix", "Andert-et-Condon"}),
+    )
+    versioned_data.add_version(
+        Millesime.M2024, frozenset({"Anglefort", "Apremont", "Aranc", "Arandas"})
+    )
+
+    FormatTest = new(
+        "FormatTest", "Versionned format", "Versionned format", versioned_data
+    )
+    format_M2023 = FormatTest(Millesime.M2023)
+    format_M2024 = FormatTest(Millesime.M2024)
+    # Valid value M2023
+    assert format_M2023.is_valid("Ambléon")
+
+    # Invalid value M2023
+    assert not format_M2023.is_valid("Anglefort")
+
+    # Valid value M2024
+    assert format_M2024.is_valid("Arandas")
+
+    # Invalid version
+    with pytest.raises(ValueError):
+        FormatTest(Millesime.M2025)  # Test with a non-existent version
