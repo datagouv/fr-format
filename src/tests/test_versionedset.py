2bb3fa1231bda4279a1400b6e569ac8a211bc8a7
sarrabah <sarrabenarbia1996@gmail.com>
2024-12-19 16:03:49 +0100

    test: Test a non sortable version

--- src/tests/test_versionedset.py
+++ src/tests/test_versionedset.py
@@ -2,57 +2,71 @@ from functools import total_ordering
 
 import pytest
 
-from frformat.versioned_set import Version, VersionedSet
+from frformat.versioned_set import VersionedSet
 
 
-def test_versionedset():
-    @total_ordering
-    class BaseVersion(Version):
-        def __init__(self, id: str) -> None:
-            self.id = id
+@total_ordering
+class SortedVersion:
+    def __init__(self, id: str) -> None:
+        self.id = id
 
-        def get_id(self) -> str:
-            return self.id
+    def get_id(self) -> str:
+        return self.id
 
-        @classmethod
-        def is_sorted(cls) -> bool:
-            return True
+    @classmethod
+    def is_sorted(cls) -> bool:
+        return True
 
-        def __eq__(self, other):
-            return self.id == other.id
+    def __eq__(self, other):
+        return self.id == other.id
 
-        def __lt__(self, other):
-            return self.id < other.id
+    def __lt__(self, other):
+        return self.id < other.id
 
-    vs = VersionedSet[BaseVersion]()
 
-    # Test empty versioned_set
+class NonSortedVersion:
+    def __init__(self, id: str) -> None:
+        self.id = id
+
+    def get_id(self) -> str:
+        return self.id
+
+    @classmethod
+    def is_sorted(cls) -> bool:
+        return False
+
+
+vs = VersionedSet[SortedVersion]()
+
+
+def test_empty_behaviour():
     assert vs.ls() == []
 
     assert vs.get_data("2021") is None
 
     assert vs.get_data("latest") is None
 
-    assert vs.add_version(BaseVersion("2025"), frozenset({"Paris"})) is None
-    assert vs.add_version(BaseVersion("2024"), frozenset({"Lyon"})) is None
+
+def test_with_data():
+    vs.add_version(SortedVersion("2025"), frozenset({"Paris"}))
+    vs.add_version(SortedVersion("2024"), frozenset({"Lyon"}))
+
+    with pytest.raises(ValueError, match="The version id 2024 already exists!"):
+        vs.add_version(SortedVersion("2024"), frozenset({"Marseille"}))
+
+    assert vs.ls() == [SortedVersion("2024"), SortedVersion("2025")]
+
     assert vs.get_data("2025") == frozenset({"Paris"})
 
-    # Duplicate id
-    with pytest.raises(ValueError, match="The version id 2024 already exists!"):
-        vs.add_version(BaseVersion("2024"), frozenset({"Marseille"}))
-
-    # sorting with version-id
-    assert vs.ls() == [BaseVersion("2024"), BaseVersion("2025")]
-    assert not vs.ls() == [BaseVersion("2025"), BaseVersion("2024")]
-
-    # "latest" version-id
     assert vs.get_data("latest") == frozenset({"Paris"})
 
-    # Immutable data
-    data = vs.get_data("2025")
     with pytest.raises(AttributeError):
-        data.add("Nice")
+        vs.add_version("1234", frozenset({"Lyon"}))  # type: ignore
 
-    # non BaseVersion type
-    with pytest.raises(AttributeError):
-        vs.add_version("1234", frozenset({"Lyon"}))
+
+def test_non_sortable_version():
+    v = VersionedSet[NonSortedVersion]()
+    v.add_version(NonSortedVersion("2021"), frozenset({"Nice"}))
+    v.add_version(NonSortedVersion("2020"), frozenset({"Jura"}))
+
+    assert v.get_data("latest") is None
