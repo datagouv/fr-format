42c539ad71274dc9e63251e96255461d252052b2
sarrabah <sarrabenarbia1996@gmail.com>
2025-01-02 14:29:43 +0100

    test: Use less examples

--- src/tests/test_geo_with_versionedset.py
+++ src/tests/test_geo_with_versionedset.py
@@ -1,6 +1,6 @@
 import pytest
 
-from frformat.geo_enum_format_with_versionedset import Millesime, new
+from frformat.geo_enum_format import Millesime, new
 from frformat.versioned_set import VersionedSet
 
 
@@ -8,18 +8,16 @@ def test_geo_enum_format_versionned():
     versioned_data = VersionedSet()
     versioned_data.add_version(
         Millesime.M2023,
-        frozenset({"Ambléon", "Ambronay", "Ambutrix", "Andert-et-Condon"}),
+        frozenset({"Ambléon", "Ambronay"}),
     )
-    versioned_data.add_version(
-        Millesime.M2024, frozenset({"Anglefort", "Apremont", "Aranc", "Arandas"})
-    )
+    versioned_data.add_version(Millesime.M2024, frozenset({"Arandas"}))
 
     FormatTest = new(
         "FormatTest", "Versionned format", "Versionned format", versioned_data
     )
     format_M2023 = FormatTest(Millesime.M2023)
-    format_M2024 = FormatTest(Millesime.M2024)
-    format_latest = FormatTest(Millesime("latest"))
+    format_M2024= FormatTest(Millesime.M2024)
+
     # Valid value M2023
     assert format_M2023.is_valid("Ambléon")
 
@@ -29,10 +27,7 @@ def test_geo_enum_format_versionned():
     # Valid value M2024
     assert format_M2024.is_valid("Arandas")
 
-    # Invalid version
+    # Test with a non-existent version
     with pytest.raises(ValueError):
-        FormatTest(Millesime.M2025)  # Test with a non-existent version
+        FormatTest("2025")  
 
-    # latest
-    assert format_latest.is_valid("Aranc")
-    assert not format_latest.is_valid("Ambléon")
