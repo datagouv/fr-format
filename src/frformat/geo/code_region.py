082371b6813c33dc5d6fd505a6b4b3a262fd9c45
sarrabah <sarrabenarbia1996@gmail.com>
2025-01-02 17:45:38 +0100

    feat: Implement Versioning inside code region

--- src/frformat/geo/code_region.py
+++ src/frformat/geo/code_region.py
@@ -1,6 +1,5 @@
-from typing import Dict, FrozenSet
-
 from frformat import geo_enum_format
+from frformat.versioned_set import VersionedSet
 
 from ..geo_enum_format import Millesime
 
@@ -30,8 +29,11 @@ CODES_REGIONS_COG_2024 = CODES_REGIONS_COG_2023
 
 name = "Code région"
 description = "Vérifie qu'il s'agit d'un code région selon le Code Officiel Géographique (cog) donné"
-all_cog_versions: Dict[Millesime, FrozenSet[str]] = {
-    Millesime.M2023: CODES_REGIONS_COG_2023,
-    Millesime.M2024: CODES_REGIONS_COG_2024,
-}
-CodeRegion = geo_enum_format.new("CodeRegion", name, description, all_cog_versions)
+
+code_region_versioned_data = VersionedSet()
+code_region_versioned_data.add_version(Millesime.M2023, CODES_REGIONS_COG_2023)
+code_region_versioned_data.add_version(Millesime.M2024, CODES_REGIONS_COG_2024)
+
+CodeRegion = geo_enum_format.new(
+    "CodeRegion", name, description, code_region_versioned_data
+)
