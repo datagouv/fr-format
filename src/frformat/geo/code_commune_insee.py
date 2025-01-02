3f9dc0ac5ca69851560e475ac2797becd02dce94
sarrabah <sarrabenarbia1996@gmail.com>
2025-01-02 17:08:12 +0100

    feat: Implement versionning inside code commune insee

--- src/frformat/geo/code_commune_insee.py
+++ src/frformat/geo/code_commune_insee.py
@@ -1,19 +1,22 @@
-from typing import Dict, FrozenSet
-
 from frformat import geo_enum_format
 from frformat.geo.code_commune_insee_frozenset import (
     CODES_COMMUNES_INSEE_COG_2023,
     CODES_COMMUNES_INSEE_COG_2024,
 )
 from frformat.geo_enum_format import Millesime
+from frformat.versioned_set import VersionedSet
 
 name = "Code commune INSEE"
 description = "Vérifie que le code commune correspond bien à un code commune INSEE pour un Code Officiel Géographique donné"
 
-all_cog_versions: Dict[Millesime, FrozenSet[str]] = {
-    Millesime.M2023: CODES_COMMUNES_INSEE_COG_2023,
-    Millesime.M2024: CODES_COMMUNES_INSEE_COG_2024,
-}
+code_commune_insee_versioned_data = VersionedSet()
+code_commune_insee_versioned_data.add_version(
+    Millesime.M2023, CODES_COMMUNES_INSEE_COG_2023
+)
+code_commune_insee_versioned_data.add_version(
+    Millesime.M2024, CODES_COMMUNES_INSEE_COG_2024
+)
+
 CodeCommuneInsee = geo_enum_format.new(
-    "CodeCommuneInsee", name, description, all_cog_versions
+    "CodeCommuneInsee", name, description, code_commune_insee_versioned_data
 )
