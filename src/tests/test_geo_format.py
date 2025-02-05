6cad3f10f0a063c3dc3c4175c22c6834d230097f
Ben Arbia Sarra <123973393+Sarrabah@users.noreply.github.com>
2025-02-05 10:42:20 +0100

    test: Update the error message
    Co-authored-by: Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>

--- src/tests/test_geo_format.py
+++ src/tests/test_geo_format.py
@@ -286,7 +286,7 @@ class TestInseeGeoFormat:
             else:
                 assert (
                     validator().get_valid_values_set() == tc["expected_result"]
-                ), f"Check that the returned data is not equal to {tc['expected_result']} when the valid_data is {tc['valid_data']} and the version is equal to {tc['version']}"
+                ), f"The returned data is not equal to {tc['expected_result']} when the valid_data is {tc['valid_data']} and the version is equal to {tc['version']}"
 
 
 class TestGeoFormat:
