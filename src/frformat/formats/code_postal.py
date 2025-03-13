5928ea9b952a90b5bb535c6596b61fe83335cfdb
sarrabah <sarrabenarbia1996@gmail.com>
2025-03-13 12:51:15 +0100

    feat: Add more code postal details

--- src/frformat/formats/code_postal.py
+++ src/frformat/formats/code_postal.py
@@ -2,7 +2,7 @@ from frformat import set_format
 from frformat.formats.code_postal_frozenset import CODES_POSTAUX
 
 name = "Code postal"
-description = "Vérifie que le code postal est bien un code postal français"
-source = ""
+description = "Vérifie que le code postal est bien un code postal français. La dernière mise à jour date du 8 février 2025"
+source = "https://datanova.laposte.fr/datasets/laposte-hexasmal"
 
 CodePostal = set_format.new("CodePostal", name, description, source, CODES_POSTAUX)
