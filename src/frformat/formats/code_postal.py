49699cd8747d74ef16389da6a64c8741880c57de
sarrabah <sarrabenarbia1996@gmail.com>
2025-03-14 10:54:59 +0100

    feat: Add the last code postal version

--- src/frformat/formats/code_postal.py
+++ src/frformat/formats/code_postal.py
@@ -1,8 +1,10 @@
 from frformat import set_format
-from frformat.formats.code_postal_frozenset import CODES_POSTAUX
+from frformat.formats.code_postal_frozenset import CODES_POSTAUX_SEMESTRE1_2025
 
 name = "Code postal"
 description = "Vérifie que le code postal est bien un code postal français. La dernière mise à jour date du 8 février 2025"
 source = "https://datanova.laposte.fr/datasets/laposte-hexasmal"
 
-CodePostal = set_format.new("CodePostal", name, description, source, CODES_POSTAUX)
+CodePostal = set_format.new(
+    "CodePostal", name, description, source, CODES_POSTAUX_SEMESTRE1_2025
+)
