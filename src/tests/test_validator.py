67145f3f4d273ee151bbf804f47e4c5f61a51b1b
sarrabah <sarrabenarbia1996@gmail.com>
2025-04-03 13:02:56 +0200

    test: Test a real case

--- src/tests/test_validator.py
+++ src/tests/test_validator.py
@@ -2,5 +2,5 @@ from frformat import Millesime, Validator
 
 
 def test_validator():
-    isvalid = Validator(Millesime.M2023).is_valid("Braund, Mr. Owen Harris")
+    isvalid = Validator(Millesime.M2023).is_valid("01001")
     assert isvalid is True
