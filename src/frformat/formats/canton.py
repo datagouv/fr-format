ef120be09b1fe42c8ce86645dde23292f991d53d
sarrabah <sarrabenarbia1996@gmail.com>
2025-04-08 15:24:46 +0200

    feat: Ignore Canton changes

--- src/frformat/formats/canton.py
+++ src/frformat/formats/canton.py
@@ -1,5 +1,5 @@
 from frformat import set_format
-from frformat.get_values_script import get_valid_values
+from frformat.formats.canton_frozenset import CANTON_COG_2023, CANTON_COG_2024
 from frformat.set_format import INSEE_SOURCE
 from frformat.versioned_set import VersionedSet
 from frformat.versions import Millesime
@@ -9,14 +9,7 @@ description = "Vérifie que le nom de canton est un canton ou pseudo-canton fran
 source = INSEE_SOURCE
 
 canton_versioned_data = VersionedSet[Millesime]()
-canton_values_2023 = get_valid_values(
-    "https://www.insee.fr/fr/statistiques/fichier/6800675/v_canton_2023.csv", "LIBELLE"
-)
-canton_versioned_data.add_version(Millesime.M2023, canton_values_2023)
-
-canton_values_2024 = get_valid_values(
-    "https://www.insee.fr/fr/statistiques/fichier/7766585/v_canton_2024.csv", "LIBELLE"
-)
-canton_versioned_data.add_version(Millesime.M2024, canton_values_2024)
+canton_versioned_data.add_version(Millesime.M2023, CANTON_COG_2023)
+canton_versioned_data.add_version(Millesime.M2024, CANTON_COG_2024)
 
 Canton = set_format.new("Canton", name, description, source, canton_versioned_data)
