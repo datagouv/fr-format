cde54b499286a860104d6804494893a069515e68
Pierlou <pierlou.ramade@data.gouv.fr>
2026-03-12 18:03:02 +0100

    feat: new id-rnb format

---
+++ frformat/formats/id_rnb.py
@@ -0,0 +1,12 @@
+from .. import CustomStrFormat, Metadata
+
+name = "ID-RNB"
+description = "Check ID-RNB validity (from Référentiel National des Bâtiments), but does not check if id exists."
+source = "https://github.com/fab-geocommuns/RNB-coeur/blob/main/app/batid/services/rnb_id.py"
+
+
+class IdRNB(CustomStrFormat):
+    metadata = Metadata(name, description, source)
+
+    def is_valid(self, value: str) -> bool:
+        return len(value) == 12 and all(c in "123456789ABCDEFGHJKMNPQRSTVWXYZ" for c in value)
